b49a69889a76763c5d51d0b0f4ac97a34f6d0dd2
Eden Ramoneda <edenlee143@gmail.com>
2020-09-13 02:03:12 +0800

    modified ui, added searchbar and scrollbar and modified mythic_notify to pnotify 1.0

--- client/client.lua
+++ client/client.lua
@@ -31,16 +31,16 @@ AddEventHandler('ViewLogs', function(allowed)
 
     else 
        -- exports['mythic_notify']:DoHudText('error', 'Only High rank officials could access this')
-       TriggerEvent("pNotify:SendNotification", {text = "Only High rank officials could access this",
-       layout = "centerLeft",
-       timeout = 2000,
-       progressBar = false,
-       type = "error",
-       animation = {
-           open = "gta_effects_fade_in",
-           close = "gta_effects_fade_out"
-       }})
-    end
+            TriggerEvent("pNotify:SendNotification", {text = "Only High rank officials could access this",
+            layout = "centerLeft",
+            timeout = 2000,
+            progressBar = false,
+            type = "error",
+            animation = {
+                open = "gta_effects_fade_in",
+                close = "gta_effects_fade_out"
+            }})
+            end
 
 end)
 
@@ -49,15 +49,15 @@ AddEventHandler('sent_police_logs', function(res)
     
     if(res == false) then
      --   exports['mythic_notify']:DoHudText('error', 'Only High rank officials could access this')
-     TriggerEvent("pNotify:SendNotification", {text = "Only High rank officials could access this",
-     layout = "centerLeft",
-     timeout = 2000,
-     progressBar = false,
-     type = "error",
-     animation = {
-         open = "gta_effects_fade_in",
-         close = "gta_effects_fade_out"
-     }})
+            TriggerEvent("pNotify:SendNotification", {text = "Only High rank officials could access this",
+            layout = "centerLeft",
+            timeout = 2000,
+            progressBar = false,
+            type = "error",
+            animation = {
+                open = "gta_effects_fade_in",
+                close = "gta_effects_fade_out"
+            }})
     else 
         SendNUIMessage({ action = 'policeLogs', result = json.encode(res) })
        -- SetNuiFocus(true, true)
